b1ba8af34b3716751c5e27be008186380ce82658
Pablo González - UC <pgs179@alumnos.unican.es>
2023-11-12 13:05:41 +0100

    Implementada la maquina de estados para el jugador

---
+++ Assets/Scripts/PlayerScripts/PlayerRunningState.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRunningState : PlayerBaseState
+{
+    public PlayerRunningState(PlayerStateMachine currentContext, PlayerStateFactory playerStateFactory)
+    : base(currentContext, playerStateFactory) { }
+
+    public override void EnterState()
+    {
+
+    }
+
+    public override void UpdateState()
+    {
+
+    }
+
+    public override void ExitState()
+    {
+
+    }
+
+    public override void CheckSwitchStates()
+    {
+
+    }
+
+    public override void InitializeSubState()
+    {
+
+    }
+}
