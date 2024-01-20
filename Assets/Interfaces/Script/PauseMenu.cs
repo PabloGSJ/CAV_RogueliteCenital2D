5f5795668000fb1262ba42c822c73270b77de628
Sandra <sao344@alumnos.unican.es>
2024-01-20 18:25:59 +0100

    All the menus finished

---
+++ Assets/Interfaces/Script/PauseMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseScreen;
+    public void Resume()
+    {
+        pauseScreen.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void BackToMenu()
+    {
+        //SceneManager.LoadScene()
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
