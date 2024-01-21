5daebd08a09a6b86bd6f66ef401d15d0320e0b05
Pablo González - UC <pgs179@alumnos.unican.es>
2024-01-21 17:57:41 +0100

    Anhadidas funciones para implementar efectos de sonido

--- Assets/SoundScripts/SoundControllerScript.cs
+++ Assets/SoundScripts/SoundControllerScript.cs
@@ -31,4 +31,43 @@ public class SoundControllerScript : MonoBehaviour
     {
         healthPotionSound.Play();
     }
+
+    // TODO:
+    // PLAYER
+    public void playPlayerDamagedSoundEffect() { }
+    public void playGameOverTuneSoundEffect() { }
+    public void playRunSoundEffect() { }
+    public void playDashSoundEffect() { }           // when player presses dash key
+    public void playHitAWallSoundEffect() { }
+
+    // WEAPONS
+    public void playGunshotSoundEffect() { }
+    public void playSwordSwingSoundEffect() { }
+    public void playPunchSoundEffect() { }
+    public void playPickupWeaponSoundEffect() { }
+    public void playBulletImpactSoundEffect() { }   // when bullets hit a wall
+    
+    // CONSUMABLES
+    public void playBulletsGroundSoundEffect() { }  // pickup GroundBullets
+    public void playBulletRoundSoundEffect() { }    // pickup BulletRound
+
+    // MAP
+    // Starting house
+    public void playExitHouseSoundEffect() { }
+    public void playJoyfulSoundEffect() { }
+    // Zone travelling
+    public void playDoorOpeningSoundEffect() { }
+    public void playCarKickstartSoundEffect() { }
+    public void playStairsDownSoundEffect() { }
+    // Entities
+    public void playChestOpenSoundEffect() { }
+    public void playBuyShopSoundEffect() { }
+
+    // ENEMIES
+
+    // BOSS
+    public void playEvilLaughSoundEffect() { }
+    public void playEnergyProjectileShotSoundEffect() { }
+    public void playFireBulletSoundEffect() { }
+    // more may come in the future, when the boss is finished
 }
