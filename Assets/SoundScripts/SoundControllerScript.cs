0267b83282f1f478ebefb3fae9fa32b2490a73f1
Sandra <sao344@alumnos.unican.es>
2024-01-23 21:29:31 +0100

    Player, items and boss sounds finished

--- Assets/SoundScripts/SoundControllerScript.cs
+++ Assets/SoundScripts/SoundControllerScript.cs
@@ -8,19 +8,111 @@ public class SoundControllerScript : MonoBehaviour
     private AudioSource coinSound;
     private AudioSource heartSound;
     private AudioSource healthPotionSound;
+    private AudioSource dashSound;
+    private AudioSource auchPlayerSound;
+    private AudioSource buySound;
+    private AudioSource chestOpenSound;
+    private AudioSource noWeaponHitSound;
+    private AudioSource swordSound;
+    private AudioSource gunSound;
+    private AudioSource pickupWeaponSound;
+    private AudioSource walkSound;
+    private AudioSource joyFulSound;
+    private AudioSource energySound;
+    private AudioSource gameOverSound;
+    private AudioSource doorOpenSound;
+    private AudioSource doorCloseSound;
+    private AudioSource stairsSound;
+    private AudioSource hitWallSound;
+    private AudioSource carStartingSound;
+    private AudioSource evilLaughSound;
+    private AudioSource pickupBulletsSound;
+
 
     public void Start()
     {
         coinSound = GameObject.Find("CoinSound").GetComponent<AudioSource>();
         heartSound = GameObject.Find("HeartSound").GetComponent<AudioSource>();
         healthPotionSound = GameObject.Find("HealthPotionSound").GetComponent<AudioSource>();
+        dashSound = GameObject.Find("DashSound").GetComponent<AudioSource>();
+        auchPlayerSound =  GameObject.Find("AuchPlayerSound").GetComponent<AudioSource>();
+        buySound = GameObject.Find("BuySound").GetComponent<AudioSource>();
+        chestOpenSound = GameObject.Find("ChestOpenSound").GetComponent<AudioSource>();
+        noWeaponHitSound = GameObject.Find("NoWeaponHitSound").GetComponent<AudioSource>();
+        swordSound = GameObject.Find("SwordSound").GetComponent<AudioSource>();
+        gunSound = GameObject.Find("GunSound").GetComponent<AudioSource>();
+        pickupWeaponSound = GameObject.Find("PickupWeaponSound").GetComponent<AudioSource>();
+        walkSound = GameObject.Find("WalkSound").GetComponent<AudioSource>();
+        joyFulSound = GameObject.Find("JoyFulSound").GetComponent<AudioSource>();
+        energySound = GameObject.Find("EnergySound").GetComponent<AudioSource>();
+        gameOverSound = GameObject.Find("GameOverSound").GetComponent<AudioSource>();
+        doorOpenSound = GameObject.Find("DoorOpenSound").GetComponent<AudioSource>();
+        doorCloseSound = GameObject.Find("DoorCloseSound").GetComponent<AudioSource>();
+        stairsSound = GameObject.Find("StairsSound").GetComponent<AudioSource>();
+        hitWallSound = GameObject.Find("HitWallSound").GetComponent<AudioSource>();
+        carStartingSound = GameObject.Find("CarStartingSound").GetComponent<AudioSource>();
+        evilLaughSound = GameObject.Find("EvilLaughSound").GetComponent<AudioSource>();
+        pickupBulletsSound = GameObject.Find("PickupBulletsSound").GetComponent<AudioSource>();
 
-        if (coinSound != null && heartSound != null&& heartSound != null)
-        {
-            Debug.Log("No problem here");
-        }
     }
 
+    // PLAYER
+    public void playPlayerDamagedSoundEffect() 
+    {
+        auchPlayerSound.Play();
+    }
+    public void playGameOverTuneSoundEffect() 
+    { 
+        gameOverSound.Play();   
+    }
+    public void playRunSoundEffect() 
+    { 
+        walkSound.Play();
+    }
+
+    public void stopRunSoundEffect()
+    {
+        walkSound.Stop();
+    }
+
+    public void playDashSoundEffect() 
+    {
+        // when player presses dash key
+        dashSound.Play();
+    }           
+    public void playHitAWallSoundEffect() { }
+
+    // WEAPONS
+    public void playGunshotSoundEffect() 
+    {
+        gunSound.Play();
+    }
+    public void playSwordSwingSoundEffect() 
+    { 
+        swordSound.Play();
+    }
+    public void playPunchSoundEffect() 
+    { 
+        noWeaponHitSound.Play();    
+    }
+    public void playPickupWeaponSoundEffect() 
+    {
+        pickupWeaponSound.Play();   
+    }
+    public void playBulletImpactSoundEffect() 
+    { 
+        hitWallSound.Play();
+    }   // when bullets hit a wall
+    
+    // CONSUMABLES
+    public void playBulletsGroundSoundEffect() 
+    {
+        pickupBulletsSound.Play();
+    }  // pickup GroundBullets
+    public void playBulletRoundSoundEffect() 
+    {
+        pickupBulletsSound.Play();  
+    }    // pickup BulletRound
 
     public void playCoinSoundEffect()
     {
@@ -37,42 +129,51 @@ public class SoundControllerScript : MonoBehaviour
         healthPotionSound.Play();
     }
 
-    // TODO:
-    // PLAYER
-    public void playPlayerDamagedSoundEffect() { }
-    public void playGameOverTuneSoundEffect() { }
-    public void playRunSoundEffect() { }
-    public void playDashSoundEffect() { }           // when player presses dash key
-    public void playHitAWallSoundEffect() { }
-
-    // WEAPONS
-    public void playGunshotSoundEffect() { }
-    public void playSwordSwingSoundEffect() { }
-    public void playPunchSoundEffect() { }
-    public void playPickupWeaponSoundEffect() { }
-    public void playBulletImpactSoundEffect() { }   // when bullets hit a wall
-    
-    // CONSUMABLES
-    public void playBulletsGroundSoundEffect() { }  // pickup GroundBullets
-    public void playBulletRoundSoundEffect() { }    // pickup BulletRound
-
     // MAP
     // Starting house
-    public void playExitHouseSoundEffect() { }
-    public void playJoyfulSoundEffect() { }
+    public void playExitHouseSoundEffect() 
+    { 
+        doorCloseSound.Play();
+    }
+    public void playJoyfulSoundEffect() 
+    { 
+        joyFulSound.Play(); 
+    }
     // Zone travelling
-    public void playDoorOpeningSoundEffect() { }
-    public void playCarKickstartSoundEffect() { }
-    public void playStairsDownSoundEffect() { }
+    public void playDoorOpeningSoundEffect() 
+    { 
+        doorOpenSound.Play();
+    }
+    public void playCarKickstartSoundEffect() 
+    { 
+        carStartingSound.Play();
+    }
+    public void playStairsDownSoundEffect() 
+    { 
+        stairsSound.Play();
+    }
+
     // Entities
-    public void playChestOpenSoundEffect() { }
-    public void playBuyShopSoundEffect() { }
+    public void playChestOpenSoundEffect() 
+    { 
+        chestOpenSound.Play();  
+    }
+    public void playBuyShopSoundEffect() 
+    { 
+        buySound.Play();
+    }
 
     // ENEMIES
 
     // BOSS
-    public void playEvilLaughSoundEffect() { }
-    public void playEnergyProjectileShotSoundEffect() { }
-    public void playFireBulletSoundEffect() { }
+    public void playEvilLaughSoundEffect() 
+    { 
+       evilLaughSound.Play();
+    }
+    public void playEnergyProjectileShotSoundEffect() 
+    {
+        energySound.Play();
+    }
+
     // more may come in the future, when the boss is finished
 }
