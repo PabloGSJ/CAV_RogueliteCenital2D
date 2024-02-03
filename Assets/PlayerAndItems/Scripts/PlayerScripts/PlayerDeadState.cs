6bb669e26665536f91d117b63b3339217b86bf37
Sandra <sao344@alumnos.unican.es>
2024-02-03 13:00:23 +0100

    Dead animation error

--- Assets/PlayerAndItems/Scripts/PlayerScripts/PlayerDeadState.cs
+++ Assets/PlayerAndItems/Scripts/PlayerScripts/PlayerDeadState.cs
@@ -8,7 +8,10 @@ public class PlayerDeadState : PlayerBaseState
     private bool _isDed;
 
     private Sprite _dead0, _dead1, _dead2, _dead3;
+    private Sprite currentSprite;
 
+    private float spriteTimer;
+    private float time;
 
 
     public PlayerDeadState(PlayerStateMachine currentContext, PlayerStateFactory playerStateFactory) : base(currentContext, playerStateFactory)
@@ -29,6 +32,7 @@ public class PlayerDeadState : PlayerBaseState
         _dead1 = _ctx.DeathSpriteSequence[1];
         _dead2 = _ctx.DeathSpriteSequence[2];
         _dead3 = _ctx.DeathSpriteSequence[3];
+        currentSprite = _dead0;
     }
 
     public override void ExitState()
@@ -47,13 +51,47 @@ public class PlayerDeadState : PlayerBaseState
 
         if (_isDed)
         {
+            Debug.Log("BUCLE");
             Deathloop();
         }
     }
 
     private void DieSequence()
     {
-        
+        if (currentSprite == _dead0)
+        {
+            currentSprite = _dead1;
+            spriteTimer = Time.time;
+            _ctx.sr.sprite = currentSprite;
+        }
+        else if (currentSprite == _dead1)
+        {
+            time = Time.time - spriteTimer;
+            if (time >= 0.5f && spriteTimer != 0)
+            {
+                currentSprite = _dead2;
+                spriteTimer = Time.time;
+                _ctx.sr.sprite = currentSprite;
+            }
+        }
+        else if (currentSprite == _dead2)
+        {
+            time = Time.time - spriteTimer;
+            if (time >= 0.5f && spriteTimer != 0)
+            {
+                currentSprite = _dead3;
+                spriteTimer = Time.time;
+                _ctx.sr.sprite = currentSprite;
+            }
+        }
+        else if (currentSprite == _dead3)
+        {
+            time = Time.time - spriteTimer;
+            if (time >= 0.5f && spriteTimer != 0)
+            {
+                _isDed = true;
+            }
+        }
     }
 
     private void Deathloop()
@@ -61,7 +99,5 @@ public class PlayerDeadState : PlayerBaseState
         _ctx.gameOverCanvas.SetActive(true);
         _ctx.gameOverAnim.SetBool("FadeIn", true);
         Time.timeScale = 0;
-
-        for (;;) { }
     }
 }
