05b73ef615f948aa8d9de9114d84ae577634099d
Pablo González - UC <pgs179@alumnos.unican.es>
2024-02-03 11:38:18 +0100

    Arreglado bug donde objetos salen por debajo de los pilares de la tienda y el selector de clases

--- Assets/PlayerAndItems/Scripts/WeaponScripts/WeaponCrowbar.cs
+++ Assets/PlayerAndItems/Scripts/WeaponScripts/WeaponCrowbar.cs
@@ -7,6 +7,8 @@ public class WeaponCrowbar : BaseMeleeWeapon
     // Because of the special properties of the Crowbar weapon, the sprite is a separate child GameObject
     public SpriteRenderer realsr;
 
+    
+
     protected override void FollowPointer()
     {
         // The crowbar sprite is at a 45 degree angle.
@@ -14,17 +16,21 @@ public class WeaponCrowbar : BaseMeleeWeapon
         rb.MoveRotation(Mathf.Atan2(_shootingVector.y, _shootingVector.x) * Mathf.Rad2Deg - 45);
     }
 
+    private void Update()
+    {
+        realsr.sortingLayerID = sr.sortingLayerID;
+        realsr.sortingOrder = sr.sortingOrder;
+    }
+
     // This is a CHAPUZA
     // Executed after picking up the weapon
     protected override void DisplayUp()
     {
-        realsr.sortingOrder = 1;
     }
 
     // Executed when dropping the weapon
     protected override void DisplayDown()
     {
-        realsr.sortingOrder = -1;
     }
 
     protected override void PlayMySoundEffect()
